18a989e67ced3075acd5de3234ebdffd1d132f8d
jonas <jonasdoevenspeck@gmail.com>
2022-02-16 17:44:34 +0100

    make classes for hand, pile and expedition

---
+++ classes.py
@@ -0,0 +1,103 @@
+class Expedition:
+
+    def __init__(self):
+        self.expeditions = {
+            'green' : [],
+            'blue' : [],
+            'yellow' : [],
+            'red' : [],
+            'white' : []
+        }
+
+    def add_card(self,card):
+        card_color = card['color']
+        card_val = card['val']
+        self.expeditions[card_color].append(card_val)
+
+    def can_build(self, cards):
+
+        
+        for card in cards:
+            card_color = card['color']
+            card_val = card['val']
+            #check if color in hand is not yet started
+            if len(self.expeditions[card_color]) == 0:
+                return True
+
+            #check if val in hand is boost and a boost is on top
+            elif card_val == 'b':
+                if self.expeditions[card_color][-1] == 'b':
+                    return True
+
+            #check if val in hand is larger than val of started exp
+            elif card_val > self.expeditions[card_color][-1]:
+                return True
+
+            else:
+                return False
+
+    def get_possible_builds(self,cards):
+        possible_builds = []
+        for card in cards:
+            card_color = card['color']
+            card_val = card['val']
+            if len(self.expeditions[card_color]) > 0:
+                top_card_exp = self.expeditions[card_color][-1]
+            #check if color in hand is not yet started
+            if len(self.expeditions[card_color]) == 0:
+                possible_builds.append(card)
+                #return True
+
+            #can always build on a boost
+            elif top_card_exp == 'b':
+                possible_builds.append(card)
+
+            #check if val in hand is larger than val of started exp in case no boost on top
+            elif isinstance(card_val, int) and (card_val > top_card_exp):
+                possible_builds.append(card)
+
+        return possible_builds    
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        id = card['id']
+        self.cards[:] = [d for d in self.cards if d.get('id') != id]
+
+
+
+class Pile:
+
+    def __init__(self):
+        self.piles = {
+            'green' : [],
+            'blue' : [],
+            'yellow' : [],
+            'red' : [],
+            'white' : []
+        }
+
+    def add_card(self,card):
+        card_color = card['color']
+        self.piles[card_color].append(card)
+
+    def draw_card(self, card):
+        id = card['id']
+        color = card['color']
+        self.piles[color][:] = [d for d in self.piles[color] if d.get('id') != id]
+
+    def get_visible_cards(self):
+        visible_cards = []
+        for color in self.piles.keys():
+            if len(self.piles[color])>0:
+                visible_cards.append(self.piles[color][-1])
+
+        return visible_cards
+
+
--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import torch
 import numpy as np
 import random
-#%% make the initial deck
+from classes import Hand, Expedition, Pile
+#%% make the initial deck and shuffle it
 
 scores = [0,0]
 
@@ -15,105 +16,75 @@ cards = [2,3,4,5,6,7,8,9,10,'b','b','b']
 card_deck = []
 discard_deck = []
 expeditions = []
+id = 1
 for color in colors:
     for card in cards:
-        card_deck.append({'color': color, 'val': card})
+        card_deck.append({'id': id,'color': color, 'val': card})
+        id +=1
 
-#%% give both players 8 random cards
-
-hands = [[],[]]
-
-for card_idx in range(start_cards):
-    hands[0].append(card_deck.pop(random.randrange(len(card_deck))))
-    hands[1].append(card_deck.pop(random.randrange(len(card_deck))))
-
-
-
+random.shuffle(card_deck)
 
 #%%
+hands = [Hand(),Hand()]
+exps = [Expedition(),Expedition()]
+centre_pile = Pile()
 play_actions = ['build','discard']
 draw_actions = ['draw_blind','draw_discard']
-
-#%%
-class Expedition:
-
-    def __init__(self):
-        self.expeditions = {
-            'green' : [],
-            'blue' : [],
-            'yellow' : [],
-            'red' : [],
-            'white' : []
-        }
-
-    def add_card(self,card):
-        self.expeditions[card['color']].append(card['val'])
-
-    def can_build(self, cards):
-
-        
-        for card in cards:
-            card_color = card['color']
-            card_val = card['val']
-            #check if color in hand is not yet started
-            if len(self.expeditions[card_color]) == 0:
-                return True
-
-            #check if val in hand is boost and a boost is on top
-            elif card_val == 'b':
-                if self.expeditions[card_color][-1] == 'b':
-                    return True
-
-            #check if val in hand is larger than val of started exp
-            elif card_val > self.expeditions[card_color][-1]:
-                return True
-
-            else:
-                return False
+#%% give both players 8 random cards
 
 
-#%%
+for card_idx in range(start_cards):
+    hands[0].add_card(card_deck.pop())
+    hands[1].add_card(card_deck.pop())
 
-exps = [Expedition(),Expedition()]Lost
 
 #%%
 player = 0
 while(len(card_deck)>0):
+    print(f'cards in deck {len(card_deck)}')
+    print(f'player {player}')
 
     exp = exps[player]
+    hand = hands[player]
 
-
-
-    #can we build?
-    if exp.can_build(hands[player]):
+    # can we build?
+    possible_builds = exp.get_possible_builds(hand.cards)
+    if len(possible_builds) > 0:
         action = play_actions[random.randint(0,1)]
     else:
         action = 'discard'
 
-    #if no cards on discard deck, draw from blind deck
-    if len(discard_deck) == 0:
+    if action == 'build':
+        card_id_played = random.randint(0,len(possible_builds)-1)
+        played_card = possible_builds[card_id_played]
+        exp.add_card(played_card) # add card to expedition
+        hand.remove_card(played_card) # remove card from hand
+    
+    elif action == 'discard':
+        card_id_played = random.randint(0,len(hand.cards)-1)
+        played_card = hand.cards[card_id_played]
+        centre_pile.add_card(played_card) # add card to centre pile
+        hand.remove_card(played_card) # remove card from hand
+
+
+    # if no cards on discard deck, draw from blind deck
+    visible_cards = centre_pile.get_visible_cards()
+    if len(visible_cards) == 0:
         draw_action = 'draw_blind'
     else:
         draw_action = draw_actions[random.randint(0,1)]
-#%%
 
-#%%
-exp = Expedition()
-exp.add_card(card_deck[0])
+    if draw_action == 'draw_blind':
+        new_card = card_deck.pop()
+        hand.add_card(new_card)
 
-
-exp.can_build(hands[0])
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-
+    elif draw_action == 'draw_discard':
+        card_id_draw = random.randint(0,len(visible_cards)-1)
+        new_card = visible_cards[card_id_draw]
+        centre_pile.draw_card(new_card) # add card to centre pile
+        hand.add_card(played_card) # remove card from hand       
+        
+    if player == 0:
+        player = 1
+    elif player == 1:
+        player = 0
